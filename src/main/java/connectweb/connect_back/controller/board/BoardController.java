ac4a98f045288ef2e5b8718150595698fcb46f15
challenger1736 <challenger1736@gmail.com>
2024-03-26 15:22:51 +0900

    2024-03-26 장혜란 최초커밋

--- src/main/java/connectweb/connect_back/controller/board/BoardController.java
+++ src/main/java/connectweb/connect_back/controller/board/BoardController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/conn")
 public class BoardController {
+    //장혜란 최초커밋
     @Autowired
     BoardService boardService;
 }
