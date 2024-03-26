f6bc93dc8e9acd6fb14c3e7c0582539e454f085a
hyo-s <sere4580@naver.com>
2024-03-26 15:28:03 +0900

    20240326_Master

--- src/main/java/connectweb/connect_back/controller/board/BoardController.java
+++ src/main/java/connectweb/connect_back/controller/board/BoardController.java
@@ -3,10 +3,13 @@ package connectweb.connect_back.controller.board;
 import connectweb.connect_back.service.board.BoardService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequestMapping("/conn")
+@CrossOrigin("http://localhost:3000")
 public class BoardController {
     @Autowired
     BoardService boardService;
