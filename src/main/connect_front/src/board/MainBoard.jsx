f149dafff56bfbbc995a8379bd1614a7c013f9ed
geonwk95 <abc777z@naver.com>
2024-03-26 18:55:44 +0900

    2024-03-26 1855

---
+++ src/main/connect_front/src/board/MainBoard.jsx
@@ -0,0 +1,38 @@
+export default function MainBoard(props){
+    return(<>
+        <section id="container">
+            <div className="innerContainer">
+                <div className="header">
+                    HEADER
+                </div>
+                <div className="content mainContent">
+                    <div className="topInfo">
+                        <div className="topImg"></div>
+                        <p>아이디</p>
+                    </div>
+                    <ul>
+                        <li>img1</li>
+                    </ul>
+                </div>
+                <div className="btmBox">
+                    <ul>
+                        <li>♥</li>
+                    </ul>
+                    <ul className="btmInfo">
+                        <li><a href="#">ksa</a></li>
+                        <li>내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용</li>
+                    </ul>
+                </div>
+                <div className="replyBox">
+                    <p>댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글</p>
+                    <p><input type="text" /></p>
+                </div>
+
+                <div className="footer">
+                    FOOTER
+                </div>
+            </div>
+        </section>
+    
+    </>)
+}
