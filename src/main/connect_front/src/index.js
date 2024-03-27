9e6d86ab2a7e64cd3e5c869c0536c3463473b89c
jang333 <tidls27@naver.com>
2024-03-27 18:21:55 +0900

    2024-03-27 김민지

--- src/main/connect_front/src/index.js
+++ src/main/connect_front/src/index.js
@@ -10,7 +10,7 @@ import SignUp from './member/SignUp';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <MainBoard />
+    <SignUp />
   </React.StrictMode>
 );
 
--- src/main/java/connectweb/connect_back/service/member/MemberService.java
+++ src/main/java/connectweb/connect_back/service/member/MemberService.java
@@ -2,6 +2,8 @@ package connectweb.connect_back.service.member;
 
 import connectweb.connect_back.model.dto.MemberDto;
 import connectweb.connect_back.model.repository.member.MemberEntityRepository;
+import connectweb.connect_back.service.FileService;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -9,13 +11,37 @@ import org.springframework.stereotype.Service;
 public class MemberService {
     @Autowired
     MemberEntityRepository memberEntityRepository;
+    @Autowired
+    FileService fileService;
+    //로그인
+    @Autowired private HttpServletRequest request;
+
+    // 회원가입
     public boolean SignupPost (MemberDto memberDto){
         System.out.println("memberDto = " + memberDto);
+
+        //프로필 파일 처리
+        String fileName="";
+        System.out.println("employeeDto.getMfile() = " + memberDto.getMfile());
+        if(!memberDto.getMfile().isEmpty()) {
+            fileName = fileService.FileUpload(memberDto.getMfile());
+            if (fileName == null) { // 업로드 성공했으면
+                return false;
+            }
+        }
+        //dto에 업로드 성공한 파일명을 대입한다
+        memberDto.setMimg(fileName);
 
         //-- Dao 안니 엔티티 이용한 레코드 저장하는 방법
         //1. 엔티티를 만든다
         //2. 리포지토리 통한 엔티티를 저장한다
         memberEntityRepository.save(memberDto.toEntity());
+        return false;
+    }
+
+    //로그인
+    public boolean loginGet (MemberDto memberDto){
+
         return false;
     }
 }
