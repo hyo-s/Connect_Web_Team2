77ea110f00935a8eac7b4ff191b8ed5881fdf42e
geonwk95 <abc777z@naver.com>
2024-03-29 18:07:46 +0900

    2024-04-01

--- src/main/connect_front/src/layout/Header.jsx
+++ src/main/connect_front/src/layout/Header.jsx
@@ -1,8 +1,12 @@
+import { Link } from "react-router-dom";
+
 export default function Header(props){
     return(<>
         <div>
             <ul>
-
+                <li><Link to="/">홈</Link></li>
+                <li><Link to="/member/signup">회원가입</Link></li>
+                <li><Link to="/member/login">로그인</Link></li>
             </ul>
         </div>
     </>)
