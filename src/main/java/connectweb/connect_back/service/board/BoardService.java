ff3418bd901b51bc70c781ee5a9c0765e91eb8e5
challenger1736 <challenger1736@gmail.com>
2024-04-01 15:56:44 +0900

    2024-03-31 로그인리액트연결 개인피드출력

--- src/main/java/connectweb/connect_back/service/board/BoardService.java
+++ src/main/java/connectweb/connect_back/service/board/BoardService.java
@@ -2,6 +2,7 @@ package connectweb.connect_back.service.board;
 
 import connectweb.connect_back.model.dto.BoardDto;
 import connectweb.connect_back.model.dto.GalleryDto;
+import connectweb.connect_back.model.dto.MemberDto;
 import connectweb.connect_back.model.entity.board.BoardEntity;
 import connectweb.connect_back.model.entity.board.GalleryEntity;
 import connectweb.connect_back.model.entity.member.MemberEntity;
@@ -9,12 +10,15 @@ import connectweb.connect_back.model.repository.board.BoardEntityRepository;
 import connectweb.connect_back.model.repository.board.GalleryEntityRepository;
 import connectweb.connect_back.model.repository.member.MemberEntityRepository;
 import connectweb.connect_back.service.FileService;
+import connectweb.connect_back.service.member.MemberService;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 
 @Service
 public class BoardService {
@@ -27,27 +31,29 @@ public class BoardService {
     GalleryEntityRepository galleryEntityRepository;
     @Autowired
     FileService fileService;
+    @Autowired
+    MemberService memberService;
+    MemberDto loginDto = memberService.loginInfo();
+    //로그인된 회원 엔티티찾기
+    Optional<MemberEntity> optionalMemberEntity = memberEntityRepository.findById(loginDto.getMno());
 
+    //게시물등록(0은 실패 1은 성공)
     @Transactional
     public int doPostBoard(BoardDto boardDto){
+        //로그인 안됐으면 실패
+        if (loginDto == null) return 0;
 
-        //------------------------------------------------
-
-        MemberEntity memberEntity = MemberEntity.builder()
-                .mno(1)
-                .mid("aaa")
-                .mpw("aaa")
-                .mname("aaa")
-                .build();
-        MemberEntity saveMember = memberEntityRepository.save(memberEntity);
+        //2. 찾은 엔티티가 존재하지 않으면 실패
+        if(!optionalMemberEntity.isPresent()) return 0;
+        //3. 멤버엔티티 꺼내기
+        MemberEntity memberEntity = optionalMemberEntity.get();
+        //글쓰기
         BoardEntity boardEntity = boardEntityRepository.save(boardDto.toEntity());
 
-        boardEntity.setMemberEntity(saveMember);
-
+        boardEntity.setMemberEntity(memberEntity);
         System.out.println("boardDto = " + boardDto);
 
         //피드이미지----------------------------------------
-
         boardDto.getGfile().forEach((uploadFile)->{
             String fileName = fileService.FileUpload(uploadFile);
 
@@ -58,10 +64,9 @@ public class BoardService {
 
             galleryEntityRepository.save(galleryEntity);
             System.out.println("fileName = " + fileName);
+            //------------------------------------------------
+            ;
         });
-
-
-
         return 0;
     }
 
@@ -75,6 +80,17 @@ public class BoardService {
         } );
         return boardDtoList;
     }
+
+    public List<Map<Object,Object>> getMyBoardList(){
+        //로그인 안됐으면 실패
+        if (loginDto == null) return null;
+
+        List<Map<Object,Object>> list = boardEntityRepository.findMyBoardList(loginDto.getMno());
+        return list;
+
+    }
+
+
 
     @Transactional
     public int doPutBoard(BoardDto boardDto){
